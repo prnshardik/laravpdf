85aec11db02462602f96f2dbec3cfd08da9099b5
prns.hardik <prns.hardik@gmail.com>
2021-08-26 12:22:23 -0700

    2021086-1222

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,82 +20,83 @@
         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+
         <script> 
             let yourstring = '';
-        </script>
-        <script>
-            
-            var url = "{{ asset('titan.pdf') }}";
 
-            PDFJS.getDocument(url)
-            .then(function(pdf) {
-                // Get div#container and cache it for later use
-                var container = document.getElementById("container");
+            $(document).ready(function(){    
+                var url = "{{ asset('titan.pdf') }}";
 
-                // Loop from 1 to total_number_of_pages in PDF document
-                for (var i = 1; i <= pdf.numPages; i++) {
+                PDFJS.getDocument(url)
+                .then(function(pdf) {
+                    // Get div#container and cache it for later use
+                    var container = document.getElementById("container");
 
-                    // Get desired page
-                    pdf.getPage(i).then(function(page) {
-                        var scale = 1.5;
-                        var viewport = page.getViewport(scale);
-                        var div = document.createElement("div");
+                    // Loop from 1 to total_number_of_pages in PDF document
+                    for (var i = 1; i <= pdf.numPages; i++) {
 
-                        // Set id attribute with page-#{pdf_page_number} format
-                        div.setAttribute("id", "page-" + (page.pageIndex + 1));
+                        // Get desired page
+                        pdf.getPage(i).then(function(page) {
+                            var scale = 1.5;
+                            var viewport = page.getViewport(scale);
+                            var div = document.createElement("div");
 
-                        // This will keep positions of child elements as per our needs
-                        div.setAttribute("style", "position: relative");
+                            // Set id attribute with page-#{pdf_page_number} format
+                            div.setAttribute("id", "page-" + (page.pageIndex + 1));
 
-                        // Append div within div#container
-                        container.appendChild(div);
+                            // This will keep positions of child elements as per our needs
+                            div.setAttribute("style", "position: relative");
 
-                        // Create a new Canvas element
-                        var canvas = document.createElement("canvas");
+                            // Append div within div#container
+                            container.appendChild(div);
 
-                        // Append Canvas within div#page-#{pdf_page_number}
-                        div.appendChild(canvas);
+                            // Create a new Canvas element
+                            var canvas = document.createElement("canvas");
 
-                        var context = canvas.getContext('2d');
-                        canvas.height = viewport.height;
-                        canvas.width = viewport.width;
+                            // Append Canvas within div#page-#{pdf_page_number}
+                            div.appendChild(canvas);
 
-                        var renderContext = {
-                            canvasContext: context,
-                            viewport: viewport
-                        };
+                            var context = canvas.getContext('2d');
+                            canvas.height = viewport.height;
+                            canvas.width = viewport.width;
 
-                        // Render PDF page
-                        page.render(renderContext)
-                        .then(function() {
-                            // Get text-fragments
-                            return page.getTextContent();
-                        })
-                        .then(function(textContent) {
-                            // Create div which will hold text-fragments
-                            var textLayerDiv = document.createElement("div");
-
-                            // Set it's class to textLayer which have required CSS styles
-                            textLayerDiv.setAttribute("class", "textLayer");
-
-                            // Append newly created div in `div#page-#{pdf_page_number}`
-                            div.appendChild(textLayerDiv);
-
-                            // Create new instance of TextLayerBuilder class
-                            var textLayer = new TextLayerBuilder({
-                                textLayerDiv: textLayerDiv, 
-                                pageIndex: page.pageIndex,
+                            var renderContext = {
+                                canvasContext: context,
                                 viewport: viewport
+                            };
+
+                            // Render PDF page
+                            page.render(renderContext)
+                            .then(function() {
+                                // Get text-fragments
+                                return page.getTextContent();
+                            })
+                            .then(function(textContent) {
+                                // Create div which will hold text-fragments
+                                var textLayerDiv = document.createElement("div");
+
+                                // Set it's class to textLayer which have required CSS styles
+                                textLayerDiv.setAttribute("class", "textLayer");
+
+                                // Append newly created div in `div#page-#{pdf_page_number}`
+                                div.appendChild(textLayerDiv);
+
+                                // Create new instance of TextLayerBuilder class
+                                var textLayer = new TextLayerBuilder({
+                                    textLayerDiv: textLayerDiv, 
+                                    pageIndex: page.pageIndex,
+                                    viewport: viewport
+                                });
+
+                                // Set text-fragments
+                                textLayer.setTextContent(textContent);
+
+                                // Render text-fragments
+                                textLayer.render();
                             });
-
-                            // Set text-fragments
-                            textLayer.setTextContent(textContent);
-
-                            // Render text-fragments
-                            textLayer.render();
                         });
-                    });
-                }
+                    }
+                });
             });
         </script>
 
